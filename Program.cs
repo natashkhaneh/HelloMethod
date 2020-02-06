471db990c6ede02e200d33d50ee523e2a9fc9a7f
Navid Atashkhaneh <natashkhaneh@usf.edu>
2020-02-06 15:48:42 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HelloMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your full name:");
+
+            try
+            {
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Hello " + name + "!");
+                Console.WriteLine("Press any key to exit the program.");
+                Console.ReadKey(true);
+            }
+
+            catch
+            {
+                Console.WriteLine("Invalid input! Press any key to exit the program and try again.");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
